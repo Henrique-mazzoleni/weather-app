579662364d3f54f2512a5f2b95cb674d87b63c73
Henrique-mazzoleni <henrique.mazzoleni@gmail.com>
2021-10-18 20:49:40 +0200

    first example running

---
+++ app.js
@@ -0,0 +1,11 @@
+console.log("Starting");
+
+setTimeout(() => {
+  console.log("Two second timer");
+}, 2000);
+
+setTimeout(() => {
+  console.log("zero second timer");
+}, 0);
+
+console.log("Stopping");
