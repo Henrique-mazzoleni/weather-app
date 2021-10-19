07edb94d56f78434537c234dd60e53d3bcda3a43
Henrique-mazzoleni <henrique.mazzoleni@gmail.com>
2021-10-19 20:10:29 +0200

    weather forcast implemented

--- app.js
+++ app.js
@@ -3,7 +3,11 @@ const request = require("request");
 const url =
   "http://api.weatherstack.com/current?access_key=869d6b1638121d75975fdcf817629bbd&query=37.8267,-122.4233";
 
-request({ url: url }, (error, response) => {
-  const data = JSON.parse(response.body);
-  console.log(data.current);
+request({ url: url, json: true }, (error, response) => {
+  const temperature = response.body.current.temperature;
+  const feels = response.body.current.feelslike;
+  const forecast = response.body.current.weather_descriptions[0];
+  console.log(
+    `${forecast}. It is currently ${temperature} degrees out. It feels like ${feels}% degrees out.`
+  );
 });
